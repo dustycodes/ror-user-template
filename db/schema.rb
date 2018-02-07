a1174cf2022ffaeec946fda0d5dd6e805c83c574
dusty <d.argyle@sarcos.com>
2018-02-06 20:19:50 -0700

    Fixing security issues, adding email address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427144033) do
+ActiveRecord::Schema.define(version: 20180207030210) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160427144033) do
     t.string "location"
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "email"
   end
 
 end
